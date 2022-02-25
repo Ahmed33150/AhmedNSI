7492240f32048b45d29ea61d0b6aad51b7ae948a
Ahmed33150 <100440178+Ahmed33150@users.noreply.github.com>
2022-02-25 19:43:42 +0100

    Add files via upload

---
+++ Code-pygames.py
@@ -0,0 +1,13 @@
+import pygame, sys
+from pygame.locals import *
+
+pygame.init()
+
+fenetre = pygame.display.set_mode((640, 480))
+
+fenetre.fill([10,186,181])
+
+pygame.display.flip()
+
+while True :
+  pass
